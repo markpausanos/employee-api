7d3a3ebcfdff08b8241284dd017f5b993e64be42
Mark Vincent Pausanos <work.markpausanos@gmail.com>
2025-05-28 13:38:01 +0800

    Refactor ExceptionHandlingMiddleware to simplify constructor and improve logging

--- EmployeeAPI/Middlewares/ExceptionHandlingMiddleware.cs
+++ EmployeeAPI/Middlewares/ExceptionHandlingMiddleware.cs
@@ -2,26 +2,17 @@ using System.Text.Json;
 
 namespace EmployeeAPI.Middlewares;
 
-public class ExceptionHandlingMiddleware
+public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
 {
-    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
-    private readonly RequestDelegate _next;
-
-    public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
-    {
-        _next = next;
-        _logger = logger;
-    }
-
     public async Task Invoke(HttpContext context)
     {
         try
         {
-            await _next(context);
+            await next(context);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Unhandled exception occurred");
+            logger.LogError(ex, "Unhandled exception occurred");
 
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
             context.Response.ContentType = "application/json";
